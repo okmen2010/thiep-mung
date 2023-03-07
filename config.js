7609c5f5b18a71146baa5e4aaea00964025e4f81
okmen2010 <91529949+okmen2010@users.noreply.github.com>
2023-03-07 22:54:55 +0700

    Add files via upload
    create card for darling

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Huyền Trang';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng công chúa';
+const contentCard = 'Chúc tình yêu của anh 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'huyentrang.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
